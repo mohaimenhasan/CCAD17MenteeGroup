34167367361b0da2dae9f19779bcc3a0328139fe
Mohaimen Khan <mohaimenkhan@microsoft.com>
2025-02-07 10:02:34 -0800

    Add new test project and update main project
    Updated the solution file to include `TestProjectCCAD17` with necessary build configurations. Modified `Program.cs` to add a namespace, `Program` class, and methods for "Hello World!" and integer division with error handling. Added `MSTestSettings.cs` for parallel test execution. Created `TestDivision` class with a test for division by zero and placeholders for other tests. Added `TestProjectCCAD17.csproj` with references to .NET SDK, MSTest, and the main project, targeting .NET 8.0.

--- CCAD17MenteeGroup/Program.cs
+++ CCAD17MenteeGroup/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CCAD17MenteeGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
---
+++ TestProjectCCAD17/TestDivision.cs
@@ -0,0 +1,35 @@
+﻿namespace TestProjectCCAD17
+{
+    [TestClass]
+    public sealed class TestDivision
+    {
+        [TestMethod]
+        public void DivideByZero_TestShouldThrow()
+        {
+            // Arrange
+            int a = 20;
+            int b = 0;
+
+            // Act
+            int result = Divide(a, b);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void DivideNonNumber()
+        {
+        }
+
+        [TestMethod]
+        public void DivideByDouble()
+        {
+        }
+
+        [TestMethod]
+        public void DivideByNegative()
+        {
+        }
+    }
+}
